d2560e1a83967c5bc34c6fd7575680913c9b7945
HanJialong <loonghan@foxmail.com>
2021-07-27 21:36:58 +0800

    added nproc in proc.c

--- kernel/proc.c
+++ kernel/proc.c
@@ -696,3 +696,14 @@ procdump(void)
     printf("\n");
   }
 }
+
+uint64 nproc(void){
+	uint64 num = 0;
+	struct proc* p;
+
+	for (p = proc; p < &proc[NPROC]; p++)
+		if (p->state != UNUSED)
+			num++;
+
+	return n;
+}
