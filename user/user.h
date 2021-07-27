049b1bd2f258f758a4ff05fd4ee799dccbfa96ce
HanJialong <loonghan@foxmail.com>
2021-07-27 20:58:50 +0800

    added 'int sysinfo(struct sysinfo *)' in user/user.h

--- user/user.h
+++ user/user.h
@@ -25,6 +25,9 @@ int sleep(int);
 int uptime(void);
 int trace(int);
 
+struct sysinfo;
+int sysinfor(struct sysinfo *);
+
 // ulib.c
 int stat(const char*, struct stat*);
 char* strcpy(char*, const char*);
